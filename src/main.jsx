78cc8efef22d13e72e63af2b533af9b0698a290a
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2026-03-27 15:36:07 +0200

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import NexTalkApp from "./nextalk-app-v2";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <NexTalkApp />
-  </React.StrictMode>
-);
